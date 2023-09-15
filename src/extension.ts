d9269a29bfa02f6c86b8b6161e2319329d0cec2d
Takatoshi Matsumoto <toqoz403@gmail.com>
2023-09-15 17:43:48 +0900

    support relative binaryPath

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as path from "path";
 import { downloadPage, lint, minimumVersion, version } from "./buf";
 import { isError } from "./errors";
 import { Formatter } from "./formatter";
@@ -6,11 +7,45 @@ import { parseLines, Warning } from "./parser";
 import { format, less } from "./version";
 
 export function activate(context: vscode.ExtensionContext) {
-  const binaryPath = vscode.workspace
-    .getConfiguration("buf")!
-    .get<string>("binaryPath");
+  const getWorkspaceFolderFsPath = () => {
+    if (vscode.workspace.workspaceFolders === undefined) {
+      console.log("workspace folders was undefined");
+      return;
+    }
+    if (vscode.workspace.workspaceFolders.length === 0) {
+      console.log("workspace folders was not set");
+      return;
+    }
+    const uri = vscode.workspace.workspaceFolders[0].uri;
+    if (uri.scheme !== "file") {
+      console.log("uri was not file: ", uri.scheme);
+      return;
+    }
+    return uri.fsPath;
+  };
+
+  const getBinaryPath = (workspaceFolderFsPath: string) => {
+    const binaryPath = vscode.workspace
+      .getConfiguration("buf")!
+      .get<string>("binaryPath");
+    if (binaryPath === undefined) {
+      console.log("buf binary path was not set");
+      return;
+    }
+
+    if (path.isAbsolute(binaryPath)) {
+      return binaryPath;
+    } else {
+      return path.join(workspaceFolderFsPath, binaryPath);
+    }
+  };
+
+  const workspaceFolderFsPath = getWorkspaceFolderFsPath();
+  if (!workspaceFolderFsPath) {
+    return;
+  }
+  const binaryPath = getBinaryPath(workspaceFolderFsPath);
   if (binaryPath === undefined) {
-    console.log("buf binary path was not set");
     return;
   }
 
@@ -66,29 +101,17 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    if (vscode.workspace.workspaceFolders === undefined) {
-      console.log("workspace folders was undefined");
-      return;
-    }
-    if (vscode.workspace.workspaceFolders.length === 0) {
-      console.log("workspace folders was not set");
-      return;
-    }
-    const uri = vscode.workspace.workspaceFolders[0].uri;
-    if (uri.scheme !== "file") {
-      console.log("uri was not file: ", uri.scheme);
+    const workspaceFolderFsPath = getWorkspaceFolderFsPath();
+    if (!workspaceFolderFsPath) {
       return;
     }
 
-    const binaryPath = vscode.workspace
-      .getConfiguration("buf")!
-      .get<string>("binaryPath");
+    const binaryPath = getBinaryPath(workspaceFolderFsPath);
     if (binaryPath === undefined) {
-      console.log("buf binary path was not set");
       return;
     }
 
-    const lines = lint(binaryPath, document.uri.fsPath, uri.fsPath);
+    const lines = lint(binaryPath, document.uri.fsPath, workspaceFolderFsPath);
     if (isError(lines)) {
       if (lines.errorMessage.includes("ENOENT")) {
         vscode.window.showInformationMessage(
