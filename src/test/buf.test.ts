ef37dbb5339fbd2ff564891245dc382f86fbbb1f
Paul Sachs <psachs@buf.build>
2024-02-01 14:58:54 -0500

    Remove more debugging

--- src/test/buf.test.ts
+++ src/test/buf.test.ts
@@ -4,12 +4,6 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 import * as buf from "../buf";
 import { getBinaryPath } from "../get-binary-path";
-import path from "path";
-
-function printFilesInDirectory(directory: string) {
-  const fs = require("fs");
-  const files = fs.readdirSync(directory);
-}
 
 suite("Buf CLI tests", () => {
   vscode.window.showInformationMessage("Start all tests.");
@@ -26,7 +20,6 @@ suite("Buf CLI tests", () => {
   test("Relative path loaded from config", async () => {
     const { binaryPath } = getBinaryPath();
     assert.ok(binaryPath);
-    printFilesInDirectory(path.resolve(binaryPath, ".."));
     const version = buf.version(binaryPath);
     if ("errorMessage" in version) {
       assert.fail(version.errorMessage);
