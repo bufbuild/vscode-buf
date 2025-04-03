c3cb65be2c3deda27318006cfa4e09d295fd3016
Paul Sachs <psachs@buf.build>
2025-04-03 15:16:43 -0400

    Add filename casing enforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config({
   ignores: [".vscode-dist"],
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  plugins: {
+    "unicorn": eslintPluginUnicorn,
+  },
   rules: {
     "@typescript-eslint/no-unused-vars": [
       "error",
@@ -13,5 +17,23 @@ export default tseslint.config({
         caughtErrorsIgnorePattern: "^_",
       },
     ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+    "unicorn/filename-case": [
+      "error",
+      {
+        "case": "kebabCase"
+      }
+    ]
   }
 });
