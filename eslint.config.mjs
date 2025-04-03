63f9d435e99379d23488ad1b7c7461cbcebf030a
Paul Sachs <psachs@buf.build>
2025-04-03 15:39:27 -0400

    Fix lint to pick up more files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,57 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 
-export default tseslint.config({
-  ignores: [".vscode-dist"],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-  plugins: {
-    unicorn: eslintPluginUnicorn,
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "variable",
-        format: ["camelCase", "PascalCase"],
-        leadingUnderscore: "allow",
-      },
-      {
-        selector: "typeLike",
-        format: ["PascalCase"],
-      },
-    ],
-    "unicorn/filename-case": [
-      "error",
-      {
-        case: "kebabCase",
-      },
-    ],
-  },
-});
+export default defineConfig(
+  globalIgnores([
+    ".vscode-test",
+    ".vscode-dist",
+    "node_modules",
+    "out",
+    "test-workspaces",
+  ]),
+  tseslint.config(
+    {
+      files: ["scripts/**/*.mjs"],
+      languageOptions: {
+        globals: globals.node,
+      }
+    }
+  ),
+  tseslint.config({
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "PascalCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+    },
+  })
+);
