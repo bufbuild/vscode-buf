3422cefbb3544055608a9fcefe14264ac18f5792
Doria Keung <doria.keung@gmail.com>
2025-07-14 20:07:09 -0400

    Fix CI, hopefully

--- test/integration/buf-binary.test.ts
+++ test/integration/buf-binary.test.ts
@@ -1,7 +1,6 @@
 import * as cp from "child_process";
 import * as fs from "fs";
 import * as os from "os";
-import * as path from "path";
 import * as vscode from "vscode";
 import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
