a036a13eedea0007d98e67a740b1489ef977413a
Doria Keung <doria.keung@gmail.com>
2025-11-20 18:29:19 -0500

    Test

--- test/playwright/extension.test.ts
+++ test/playwright/extension.test.ts
@@ -1,5 +1,8 @@
 import * as fs from "node:fs";
+import * as os from "node:os";
 import * as path from "node:path";
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/node";
 import { expect, type Page, test } from "./base-test";
 
 const baseBufYaml = `version: v2`;
@@ -361,6 +364,81 @@ message UserEvent {
 `;
 
 /**
+ * The test asset download URL. We use a single test download URL for all assets so that
+ * tests are consistent across platforms.
+ */
+const assetDownloadURL =
+  "https://api.github.com/repos/bufbuild/buf/releases/assets/";
+const downloadBinPath = "test/workspaces/empty-single/node_modules/@bufbuild/";
+export const githubReleaseURL =
+  "https://api.github.com/repos/bufbuild/buf/releases/";
+
+/**
+ * msw stub handlers for GitHub releases API.
+ */
+const handlers = [
+  http.get(`${githubReleaseURL}latest`, () => {
+    return HttpResponse.json({
+      name: "v1.54.0",
+      tag_name: "v1.54.0",
+      assets: [
+        {
+          name: "buf-Darwin-arm64",
+          url: `${assetDownloadURL}buf-darwin-arm64`,
+        },
+        {
+          name: "buf-Darwin-x86_64",
+          url: `${assetDownloadURL}buf-darwin-x64`,
+        },
+        {
+          name: "buf-Linux-x86_64",
+          url: `${assetDownloadURL}buf-linux-x64`,
+        },
+        {
+          name: "buf-Linux-aarch64",
+          url: `${assetDownloadURL}buf-linux-aarch64`,
+        },
+        {
+          name: "buf-Windows-x86_64.exe",
+          url: `${assetDownloadURL}buf-win32-x64`,
+        },
+        {
+          name: "buf-Windows-arm64.exe",
+          url: `${assetDownloadURL}buf-win32-arm64`,
+        },
+      ],
+    });
+  }),
+  http.get(`${assetDownloadURL}:platformKey`, ({ params }) => {
+    try {
+      const bin = fs.readFileSync(
+        os.platform() === "win32"
+          ? path.resolve(
+              __dirname,
+              `../../../${downloadBinPath}${params.platformKey}/bin/buf.exe`
+            )
+          : `${downloadBinPath}${params.platformKey}/bin/buf`
+      );
+      const stream = new ReadableStream({
+        start(controller) {
+          controller.enqueue(bin);
+          controller.close();
+        },
+      });
+      return new HttpResponse(stream, {
+        headers: {
+          "content-type": "application/octet-stream",
+        },
+      });
+    } catch (e) {
+      return HttpResponse.json({ error: e }, { status: 404 });
+    }
+  }),
+];
+
+const server = setupServer(...handlers);
+
+/**
  * A test that provides a fixture for initialising project files.
  *
  * @param fileContents is a Record<string, string> of file names to contents.
@@ -406,11 +484,16 @@ export const extensionTest = test.extend<{
 });
 
 extensionTest.describe("status bar", async () => {
+  server.listen();
+  extensionTest.afterEach(async () => {
+    server.resetHandlers();
+  });
   extensionTest.use({ activateFileName: "example.proto" });
   extensionTest("toolbar displays successful running lsp", async ({ page }) => {
     // Validate that buf lsp is displaying success in the status bar
     await expect(page.getByRole("button", { name: "check Buf" })).toBeVisible();
   });
+  server.close();
 });
 
 extensionTest.describe("command palette", async () => {
